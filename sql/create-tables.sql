3597190fe7908bd155a3824b6f595c5f91a6767c
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-02 15:11:41 -0500

    added courses table for tracking user registered courses

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -5,8 +5,17 @@ CREATE DATABASE IF NOT EXISTS quizbud;
 ALTER DATABASE quizbud default character set utf8mb4 COLLATE utf8mb4_unicode_ci;
 use quizbud;
 
-DROP TABLES IF EXISTS users, quizes, questions, classes, schools, attempts, answers, attempts_answers ;
+DROP TABLES IF EXISTS users, quizes, questions, classes, schools, attempts, answers, attempts_answers, courses ;
 
+
+
+
+CREATE TABLE schools (
+id bigint not null auto_increment,
+schoolname varchar(200) not null,
+domainname varchar(100) not null,
+PRIMARY KEY (id)
+);
 
 CREATE TABLE users (
 id BIGINT NOT NULL AUTO_INCREMENT,
@@ -18,16 +27,14 @@ registrationDate DATETIME NOT NULL,
 lastOnline DATETIME null default null,
 profileSummary tinytext null default null,
 profileLongtext text null default null,
+school BIGINT not null,
 PRIMARY KEY (id),
-UNIQUE INDEX unique_email (email ASC)
-);
-
-
-CREATE TABLE schools (
-id bigint not null auto_increment,
-schoolname varchar(200) not null,
-domainname varchar(100) not null,
-PRIMARY KEY (id)
+UNIQUE INDEX unique_email (email ASC),
+CONSTRAINT fk_users_school
+FOREIGN KEY(school)
+REFERENCES schools(id)
+ON delete no action
+on update no action
 );
 
 CREATE TABLE classes (
@@ -134,8 +141,19 @@ constraint fk_attempts_answers_answer
     references quizbud.answers (id)
     on delete no action
     on update no action);
-
-
+    
+CREATE TABLE courses (
+userid BIGINT,
+classid BIGINT,
+PRIMARY KEY (userid, classid),
+constraint fk_courses_userid
+	foreign key (userid)
+    references quizbud.users (id),
+constraint fk_courses_classid
+	foreign key (classid)
+    references quizbud.classes (id)
+    on delete no action
+    on update no action);
 
     
 
