6f0370e9482e1b9165b7db759e67d9ed5889798c
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-03 13:00:50 -0500

    serverconnect script and close connect in footer

--- serverconnect.php
+++ serverconnect.php
@@ -1,19 +1,11 @@
 <?php
-$user = $_POST["email"];
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-// Initiate a connection with the database
-$connection = new mysqli($servername, $username, $password);
-
-// Verify if connection is working
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$sql = "Use assignment2;
-SELECT * FROM users
-WHERE username = $user"
+    $host="127.0.0.1";
+    $port=3306;
+    $socket="";
+    $user="root";
+    $password="";
+    $dbname="assignment2";
+    
+    $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+        or die ('Could not connect to the database server' . mysqli_connect_error());
 ?>
