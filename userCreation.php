b2e3278b5b4eaff855c89b14c076e8cc82a5706e
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-03 14:30:15 -0500

    theoretically, creates new users.

--- userCreation.php
+++ userCreation.php
@@ -1,23 +1,31 @@
 <?php
 include_once('serverconnect.php');
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $password = $_POST['password'];
 
-$email = $_POST['email'];
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$password = $_POST['password'];
+    $verifyUser = "Select * from users where 
+    email = $email";
+    $verifyCheck = mysqli_query($con, $verifyCheck);
+    
+    $anythingThere = mysqli_num_rows($verifyCheck);
+    if ($row < 1){
+        mysqli_query($con,"INSERT INTO users   
+        (firstname, lastname, email, password,school) 
+        VALUES ($firstname, $lastname, $email,
+        $password, 1)");
+        
+    }
 
-
-$sql = "INSERT INTO users 
-(firstname, lastname, email, password, 
-registrationdate, lastonline, school) 
-VALUES ($firstname, $lastname, $email,
-$password, sysdate, 1)";
     
 
 if ($con->query($sql) == TRUE) {
     echo "New user created successfully";
 }else {
     echo "Error: " . $sql . "<br>" . $con->error;
+}
 }
 $con->close();
 ?>
