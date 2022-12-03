91fa5ce5d9f396ad473d6364fa78a6747bbfeb13
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-03 12:54:13 -0500

    start of dashboard

---
+++ dashboard.php
@@ -0,0 +1,31 @@
+<?php session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuizBuddy - Dashboard</title>
+</head>
+<body>
+    <?php
+    include_once('header.php');
+    ?>
+    
+<div class="dashboard">
+    <div><h3>Courses</h3>
+    <?php
+    
+    ?>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+    <?php
+    include_once('footer.php');
+    ?>
+</body>
+</html>
