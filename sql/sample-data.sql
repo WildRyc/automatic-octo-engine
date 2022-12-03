1a5eae288a7c3f274fe32db7497ed177c095d6da
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-03 14:20:54 -0500

    new default sample data

--- sql/sample-data.sql
+++ sql/sample-data.sql
@@ -3,8 +3,11 @@ INSERT INTO schools VALUES
 ( 2, "University of Ottawa", "uottawa.ca"),
 ( 3, "Carleton University", "carleton.ca");
 
-INSERT INTO users VALUES
-(1, "Renato", "Simoes", "simo0138@algonquinlive.com", "Password", sysdate(), sysdate(), null, null, 1);
+INSERT INTO users 
+(firstname, lastname, email, password, school)
+VALUES
+("Renato", "Simoes", "simo0138@algonquinlive.com", 
+"Password", 1);
 
 INSERT INTO classes values
 (1, 1, "CST8285", "Web Programming", sysdate(), 1);
