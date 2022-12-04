f233af949de112643be249087334ea44ebdb0f16
Jordan Chen <chen0874@algonquinlive.com>
2022-12-04 18:01:24 -0500

    created validation.js

---
+++ validation.js
@@ -0,0 +1,16 @@
+function validateForm() {
+    let x = document.forms["myForm"]["username"].value;
+    if (x == "") {
+      alert("Username must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+  let x = document.forms["myForm"]["password"].value;
+    if (x == "") {
+      alert("Password must be filled out");
+      return false;
+    }
+  }
+
