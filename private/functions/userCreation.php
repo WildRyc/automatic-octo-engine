0538715d67a0947263f35d75744f892a098c6a71
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-04 18:52:39 -0500

    create user, go to dashboard

--- private/functions/userCreation.php
+++ private/functions/userCreation.php
@@ -6,31 +6,43 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $lastname = $_POST['lastname'];
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
-    $emailCheck ="";
-    $verifyUser = "Select email from users where 
+    
+    $verifyUser = "Select * from users where 
     email = ?";
-    $stmt = $con->prepare($verifyUser); 
+    $stmt = $con->prepare($verifyUser);
     $stmt->bind_param("s",$email);
     $stmt->execute();
     $stmt->store_result();
+    $stmt->fetch();
     if ($stmt->num_rows == 0 ) {
-    $creation = "INSERT INTO users   
+        $creation = "INSERT INTO users   
         (firstname, lastname, email, password, school) 
         VALUES (?, ?, ?,
         ?, 1)";
-    $stmt->close(); 
-
-    $stmt = $con->prepare($creation); 
-    $stmt->bind_param("ssss",$firstname,$lastname,$email,$password);
-    $stmt->execute();
-    if ($stmt->execute() === true ) {
-        echo "New user created successfully";
-        header('location:dashboard.php');
-    } else {
-        header('location:index.php');
-        echo "Error: " . $creation . "<br>" . $con->error;
-    }
-    
+        
+        
+        $stmt2 = $con->prepare($creation); 
+        $stmt2->bind_param("ssss",$firstname,$lastname,$email,$password);
+        $stmt2->execute();
+        $result = $con->query("Select * from users where 
+        email = '$email'");
+        if ($result->num_rows == 1 ) {
+            $values= $result->fetch_assoc();
+            session_start();
+            $_SESSION['firstname'] = $firstname;
+            $_SESSION['email'] = $email;
+            $_SESSION['lastname'] = $lastname;
+            $_SESSION['id'] = $values['id'];
+            $_SESSION['school'] = $values['schoolid'];
+            $_SESSION['classlist'] = array();
+            session_write_close();
+            header('location:../../public/dashboard.php');
+            echo "New user created successfully";
+        } else {
+            header('location:../../../index.php');
+            echo "Error: " . $creation . "<br>" . $con->error;
+        }
+        
     } else {
         echo '<script type="text/javascript">
         window.alert("User with this email already exists!")
