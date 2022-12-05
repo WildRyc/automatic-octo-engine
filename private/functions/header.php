3beaf66afa3a4e632dbccd55e69b0653d2488327
Kyle Ryc <kyle.ryc@gmail.com>
2022-12-04 21:08:32 -0500

    updated header to look less ridiculous

--- private/functions/header.php
+++ private/functions/header.php
@@ -1,36 +1,27 @@
-<header>
-<button style="float:right" a href="<?php echo 'index.php'; ?>">Home</a></button>
-<img src="images/Logo.png" alt="Logo">
-<div style="float:right">
-    <form name="logoutheader" method="post" action="../private/functions/userlogout.php">
-    <button type="submit">Logout</button>
-</form>
-</div>
-
-<html lang="en">
-  <head>
-    
     <meta charset="utf-8">
     <link rel="stylesheet" href="<?php echo 'style.css'; ?>" />
+    <link rel="icon" type="image/x-icon" href="./images/favicon.png">
+    <link rel="stylesheet" href="./stylesheet/style.css">
   </head>
-
   <body>
     <header>
-      <h3>Your Credentials</h3>
-      <hr>
-    </header>
-
-<?php
+      <?php
 $firstname = $_SESSION['firstname'];
 $lastname = $_SESSION['lastname'];
 $email = $_SESSION['email'];
 $id = $_SESSION['id'];
-echo nl2br("Name: $firstname $lastname\n E-mail: $email\r\n ID Number: $id");  
-?>
-<link rel="icon" type="image/x-icon" href="./images/favicon.png">
-<link rel="stylesheet" href="./stylesheet/style.css">
-</head>
-<body>
-<hr>
+// echo nl2br("Name: $firstname $lastname\n E-mail: $email\r\n ID Number: $id");  
+?>  
+        <div class="brandLogo"><a href="./dashboard.php">QUIZ BUDDY</a>
+</div>
+        <?php
+    print_r("<div id='username'>$firstname $lastname</div>");
+    print_r("<div id='idnumber'>User ID: $id  </div>");
+    ?><div class="logoutButton"><form name="logoutheader" method="post" action="../private/functions/userlogout.php">
+    <button type="submit">Logout</button>
+    </form></div>
+
+</header>
+
 
 
